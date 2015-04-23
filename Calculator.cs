30e36b210d8da2405af4d0ffe34649498c690f9e
Pavel Egorov <xoposhiy@mail.ru>
2015-04-23 20:41:00 +0500

    Calculator does not fail any more on error user input.

--- Calculator.cs
+++ Calculator.cs
@@ -6,23 +6,26 @@ namespace Kontur.Courses.Git
 
 		public Maybe<double> Calculate(string[] args)
 		{
-			switch (args.Length)
+			if (args.Length == 0)
+				return lastResult;
+			if (args.Length == 1)
+				return lastResult = TryParseDouble(args[0]);
+			if (args.Length == 3)
 			{
-				case 0:
-					return lastResult;
-				case 1:
-					return lastResult = double.Parse(args[0]);
-				case 2:
-					if (!lastResult.HasValue)
-						return lastResult = Maybe<double>.FromError("Last result is abcent");
-					var v = double.Parse(args[1]);
-					return lastResult = Execute(args[0], lastResult.Value, v);
-				case 3:
-					var v1 = double.Parse(args[0]);
-					var v2 = double.Parse(args[2]);
-					return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				return lastResult = Execute(args[1], v1.Value, v2.Value);
 			}
 			return Maybe<double>.FromError("Error input");
+		}
+
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
 		}
 
 		private Maybe<double> Execute(string op, double v1, double v2)
